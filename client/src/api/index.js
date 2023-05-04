d47f0e28c9947c3e446c3368927d83d74e9bfca2
Eddy Gonzalez <gonzalezeddyan@gmail.com>
2023-05-04 14:39:27 -0400

    changes for deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL: 'http://localhost:5000' })
+const API = axios.create({baseURL: '/api' })
 
 //send token back to middleware for verification and authorization
 API.interceptors.request.use((req) => {
