4c0320264ca25175668150a03aee43d5fd429c1b
Eddy Gonzalez <gonzalezeddyan@gmail.com>
2023-05-05 10:04:44 -0400

    modified api end point

--- server/index.js
+++ server/index.js
@@ -14,8 +14,8 @@ app.use(bodyParser.json({limit: '30mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors())
 
-app.use('/posts', postRoutes)
-app.use('/user', userRoutes)
+app.use('/api/posts', postRoutes)
+app.use('/api/user', userRoutes)
 
 const CONNECTION_URI = process.env.CONNECTION_URI
 const PORT = process.env.PORT
